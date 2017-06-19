72d6a36a00b1c6e8d301c38b1d631d073e2e22bb
tietang <wtt.2012@gmail.com>
2017-06-19 14:36:54 +0800

    add empty new

--- ConfigSource.go
+++ ConfigSource.go
@@ -1,232 +1,241 @@
 package props
 
 import (
-	"errors"
-	"strings"
-	"time"
-	"github.com/samuel/go-zookeeper/zk"
-	"regexp"
+    "errors"
+    "strings"
+    "time"
+    "github.com/samuel/go-zookeeper/zk"
+    "regexp"
 )
 
 var reg = regexp.MustCompile("\\$\\{(.*)}")
 
 type ConfigSource interface {
-	Name() string
-	//
-	Get(key string) (string, error)
-	GetInt(key string) (int, error)
-	GetBool(key string) (bool, error)
-	GetFloat64(key string) (float64, error)
-	//
-	GetDefault(key, defaultValue string) string
-	GetIntDefault(key string, defaultValue int) int
-	GetBoolDefault(key string, defaultValue bool) bool
-	GetFloat64Default(key string, defaultValue float64) float64
-	//
-	Set(key, val string)
-	SetAll(values map[string]string)
-	Keys() []string
+    Name() string
+    //
+    Get(key string) (string, error)
+    GetInt(key string) (int, error)
+    GetBool(key string) (bool, error)
+    GetFloat64(key string) (float64, error)
+    //
+    GetDefault(key, defaultValue string) string
+    GetIntDefault(key string, defaultValue int) int
+    GetBoolDefault(key string, defaultValue bool) bool
+    GetFloat64Default(key string, defaultValue float64) float64
+    //
+    Set(key, val string)
+    SetAll(values map[string]string)
+    Keys() []string
 }
 
 //
 type CompositeConfigSource struct {
-	name          string
-	ConfigSources []ConfigSource //Set
+    name          string
+    ConfigSources []ConfigSource //Set
+}
+
+func NewEmptyCompositeConfigSource() *CompositeConfigSource {
+    s := &CompositeConfigSource{
+        ConfigSources: make([]ConfigSource, 0),
+    }
+    s.name = "CompositeConfigSource"
+
+    return s
 }
 
 func NewDefaultCompositeConfigSource(configSources []ConfigSource) *CompositeConfigSource {
-	s := &CompositeConfigSource{
-		ConfigSources: configSources,
-	}
-	s.name = "CompositeConfigSource"
+    s := &CompositeConfigSource{
+        ConfigSources: configSources,
+    }
+    s.name = "CompositeConfigSource"
 
-	return s
+    return s
 }
 
-func NewCompositeConfigSource(name string,configSources []ConfigSource) *CompositeConfigSource {
-	s := &CompositeConfigSource{
-		ConfigSources: configSources,
-	}
-	s.name = "CompositeConfigSource"
+func NewCompositeConfigSource(name string, configSources []ConfigSource) *CompositeConfigSource {
+    s := &CompositeConfigSource{
+        ConfigSources: configSources,
+    }
+    s.name = "CompositeConfigSource"
 
-	return s
+    return s
 }
 
 func NewZookeeperCompositeConfigSource(contexts []string, connStr []string, timeout time.Duration) *CompositeConfigSource {
 
-	conn, _, err := zk.Connect(connStr, timeout)
-	if err != nil {
-		panic(err)
-	}
-	return NewZookeeperCompositeConfigSourceByConn(contexts, conn)
+    conn, _, err := zk.Connect(connStr, timeout)
+    if err != nil {
+        panic(err)
+    }
+    return NewZookeeperCompositeConfigSourceByConn(contexts, conn)
 }
 
 func NewZookeeperCompositeConfigSourceByConn(contexts []string, conn *zk.Conn) *CompositeConfigSource {
 
-	s := &CompositeConfigSource{}
-	for _, context := range contexts {
-		zkms := NewZookeeperConfigSource("zk:"+context, context, conn)
-		s.Add(zkms)
-	}
-	s.name = "Zookeeper"
+    s := &CompositeConfigSource{}
+    for _, context := range contexts {
+        zkms := NewZookeeperConfigSource("zk:"+context, context, conn)
+        s.Add(zkms)
+    }
+    s.name = "Zookeeper"
 
-	return s
+    return s
 }
 
 func (s *CompositeConfigSource) Name() string {
-	return s.name
+    return s.name
 }
 func (s *CompositeConfigSource) Add(ms ConfigSource) {
-	for _, s := range s.ConfigSources {
-		if ms.Name() == s.Name() {
-			return
-		}
-	}
-	s.ConfigSources = append(s.ConfigSources, ms)
+    for _, s := range s.ConfigSources {
+        if ms.Name() == s.Name() {
+            return
+        }
+    }
+    s.ConfigSources = append(s.ConfigSources, ms)
 
 }
 
 func (s *CompositeConfigSource) Get(key string) (string, error) {
-	//var value string;
-	//var found bool;
-	//s.ConfigSources.ForEach(func(mapSource interface{}, ok bool) {
-	//    if ok {
-	//        ms := mapSource.(ConfigSource)
-	//        v, err := ms.Get(key)
-	//        if err == nil {
-	//            value = v
-	//            found = ok
-	//            return -1
-	//        }
-	//    }
-	//})
-	//
-	//if found {
-	//    return value, nil
-	//}
-	//
-	for _, s := range s.ConfigSources {
-		v, err := s.Get(key)
-		if err == nil {
-			return v, nil
-		}
-	}
-	return "", errors.New("not exists for key: " + key)
+    //var value string;
+    //var found bool;
+    //s.ConfigSources.ForEach(func(mapSource interface{}, ok bool) {
+    //    if ok {
+    //        ms := mapSource.(ConfigSource)
+    //        v, err := ms.Get(key)
+    //        if err == nil {
+    //            value = v
+    //            found = ok
+    //            return -1
+    //        }
+    //    }
+    //})
+    //
+    //if found {
+    //    return value, nil
+    //}
+    //
+    for _, s := range s.ConfigSources {
+        v, err := s.Get(key)
+        if err == nil {
+            return v, nil
+        }
+    }
+    return "", errors.New("not exists for key: " + key)
 }
 
 func (s *CompositeConfigSource) GetInt(key string) (int, error) {
-	for _, s := range s.ConfigSources {
-		v, err := s.GetInt(key)
-		if err == nil {
-			return v, nil
-		}
-	}
-	return 0, errors.New("not exists for key: " + key)
+    for _, s := range s.ConfigSources {
+        v, err := s.GetInt(key)
+        if err == nil {
+            return v, nil
+        }
+    }
+    return 0, errors.New("not exists for key: " + key)
 }
 
 func (s *CompositeConfigSource) GetBool(key string) (bool, error) {
-	for _, s := range s.ConfigSources {
-		v, err := s.GetBool(key)
-		if err == nil {
-			return v, nil
-		}
-	}
-	return false, errors.New("not exists for key: " + key)
+    for _, s := range s.ConfigSources {
+        v, err := s.GetBool(key)
+        if err == nil {
+            return v, nil
+        }
+    }
+    return false, errors.New("not exists for key: " + key)
 }
 
 func (s *CompositeConfigSource) GetFloat64(key string) (float64, error) {
-	for _, s := range s.ConfigSources {
-		v, err := s.GetFloat64(key)
-		if err == nil {
-			return v, nil
-		}
-	}
-	return 0.0, errors.New("not exists for key: " + key)
+    for _, s := range s.ConfigSources {
+        v, err := s.GetFloat64(key)
+        if err == nil {
+            return v, nil
+        }
+    }
+    return 0.0, errors.New("not exists for key: " + key)
 }
 
 func (s *CompositeConfigSource) GetDefault(key string, defaultValue string) string {
-	v, err := s.Get(key)
-	if err != nil {
-		return defaultValue
-	}
-	return v
+    v, err := s.Get(key)
+    if err != nil {
+        return defaultValue
+    }
+    return v
 
 }
 
 func (s *CompositeConfigSource) GetIntDefault(key string, defaultValue int) int {
-	v, err := s.GetInt(key)
-	if err != nil {
-		return defaultValue
-	}
-	return v
+    v, err := s.GetInt(key)
+    if err != nil {
+        return defaultValue
+    }
+    return v
 
 }
 
 func (s *CompositeConfigSource) GetBoolDefault(key string, defaultValue bool) bool {
-	v, err := s.GetBool(key)
-	if err != nil {
-		return defaultValue
-	}
-	return v
+    v, err := s.GetBool(key)
+    if err != nil {
+        return defaultValue
+    }
+    return v
 }
 
 func (s *CompositeConfigSource) GetFloat64Default(key string, defaultValue float64) float64 {
-	v, err := s.GetFloat64(key)
-	if err != nil {
-		return defaultValue
-	}
-	return v
+    v, err := s.GetFloat64(key)
+    if err != nil {
+        return defaultValue
+    }
+    return v
 }
 
 func (s *CompositeConfigSource) Set(key, val string) {
-	panic(errors.New("Unsupported operation"))
+    panic(errors.New("Unsupported operation"))
 }
 
 func (s *CompositeConfigSource) SetAll(values map[string]string) {
-	panic(errors.New("Unsupported operation"))
+    panic(errors.New("Unsupported operation"))
 }
 
 func (s *CompositeConfigSource) Keys() []string {
-	keys := make([]string, 0)
-	for _, s := range s.ConfigSources {
-		ks := s.Keys()
-		for _, k := range ks {
-			keys = append(keys, k)
-		}
+    keys := make([]string, 0)
+    for _, s := range s.ConfigSources {
+        ks := s.Keys()
+        for _, k := range ks {
+            keys = append(keys, k)
+        }
 
-	}
-	return keys
+    }
+    return keys
 }
 
 func (s *CompositeConfigSource) GetValue(key string) (string, error) {
-	v, err := s.Get(key)
-	if err == nil {
-		if reg.MatchString(v) {
-			return s.calculateValue(v)
-		}
-		return v, nil
-	}
-	return v, err
+    v, err := s.Get(key)
+    if err == nil {
+        if reg.MatchString(v) {
+            return s.calculateValue(v)
+        }
+        return v, nil
+    }
+    return v, err
 }
 
 func (s *CompositeConfigSource) calculateValue(value string) (string, error) {
-	sub := reg.FindStringSubmatch(value)
-	if len(sub) == 0 {
-		return value, nil
-	}
-	defaultValue := ""
-	for _, k := range sub {
+    sub := reg.FindStringSubmatch(value)
+    if len(sub) == 0 {
+        return value, nil
+    }
+    defaultValue := ""
+    for _, k := range sub {
 
-		keys := strings.Split(k, ":")
-		if len(keys) > 1 {
-			k = keys[0]
-			defaultValue = keys[1]
-		}
-		v, err := s.Get(k)
-		if err == nil {
-			return v, nil
-		}
-	}
+        keys := strings.Split(k, ":")
+        if len(keys) > 1 {
+            k = keys[0]
+            defaultValue = keys[1]
+        }
+        v, err := s.Get(k)
+        if err == nil {
+            return v, nil
+        }
+    }
 
-	return defaultValue, errors.New("not exists")
+    return defaultValue, errors.New("not exists")
 }
