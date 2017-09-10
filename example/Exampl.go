0a355dec6a3d24de7079a07cbda5b269385e2dd3
Tietang Wang <tietang@MacBook-Pro.local>
2017-09-10 12:38:22 +0800

    fixed example

--- example/Exampl.go
+++ example/Exampl.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/tietang/props"
+    "fmt"
 )
 
 func main() {
@@ -9,4 +10,5 @@ func main() {
     root := "config/app1/dev"
     address := "127.0.0.1:8500"
     p := props.NewConsulIniConfigSourceByName("consul-ini", address, root)
+    fmt.Println(p.Get(""))
 }
