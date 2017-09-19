759bed85253af40c7f7d0fcd57074086315ff235
tietang <wtt.2012@gmail.com>
2017-09-19 14:56:40 +0800

    update example

--- example/Exampl.go
+++ example/Exampl.go
@@ -10,7 +10,7 @@ func main3() {
 
     root := "config/app1/dev"
     address := "127.0.0.1:8500"
-    
+
     p := props.NewConsulPropsConfigSourceByName("consul-props", address, root, 10*time.Second)
     fmt.Println(p.Get(""))
 }
