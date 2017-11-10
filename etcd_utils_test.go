1b90487057e09c5580ca3939e415d6c0aa4d8fad
tietang <wtt.2012@gmail.com>
2017-11-10 11:19:07 +0800

    etcd address to localhost

--- etcd_utils_test.go
+++ etcd_utils_test.go
@@ -10,8 +10,8 @@ var etcd_mock_started = false
 var etcdAddress string
 
 func init() {
-    etcdAddress = "http://172.16.1.248:2379"
-    //etcdAddress = "http://127.0.0.1:2379"
+    //etcdAddress = "http://172.16.1.248:2379"
+    etcdAddress = "http://127.0.0.1:2379"
     GetOrNewMockTestEtcd(etcdAddress)
     if !etcd_mock_started {
         go testEtcd.StartMockEtcd()
