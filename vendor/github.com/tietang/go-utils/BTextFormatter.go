3d760c4b38f2ba0d12431eac36e27b62ce429b07
tietang <wtt.2012@gmail.com>
2018-07-04 16:36:50 +0800

    updated ci config

--- vendor/github.com/tietang/go-utils/BTextFormatter.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/sirupsen/logrus"
-
-type BTextFormatter struct {
-	logrus.TextFormatter
-}
--- vendor/github.com/tietang/go-utils/strutils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "regexp"
-
-func splitByEmptyNewline(str, reg string) []string {
-
-	return regexp.MustCompile(reg).Split(str, -1)
-
-}
