8093999ac13e834b8980b090d9d2fbfe56c5e715
tietang <wtt.2012@gmail.com>
2019-03-21 10:39:22 +0800

    add get current filepath function

--- kvs/helper.go
+++ kvs/helper.go
@@ -1,63 +1,75 @@
 package kvs
 
 import (
-    "fmt"
-    "io"
-    "os/exec"
-    "bufio"
-    "time"
-    "strings"
-    "strconv"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func ToDuration(v string) (time.Duration, error) {
 
-    v = strings.ToLower(v)
-    i, err := strconv.ParseInt(v, 10, 64)
-    if err == nil {
-        return time.Duration(i) * time.Second, nil
-    }
-    return time.ParseDuration(v)
+	v = strings.ToLower(v)
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err == nil {
+		return time.Duration(i) * time.Second, nil
+	}
+	return time.ParseDuration(v)
 
-    //if strings.LastIndex(v, TIME_MS) > 0 {
-    //    i, err := strconv.ParseInt(strings.TrimSuffix(v, TIME_MS), 10, 0)
-    //    return time.Duration(i) * time.Millisecond, err
-    //} else {
-    //    i, err := strconv.ParseInt(strings.TrimSuffix(v, TIME_S), 10, 0)
-    //    return time.Duration(i) * time.Second, err
-    //}
+	//if strings.LastIndex(v, TIME_MS) > 0 {
+	//    i, err := strconv.ParseInt(strings.TrimSuffix(v, TIME_MS), 10, 0)
+	//    return time.Duration(i) * time.Millisecond, err
+	//} else {
+	//    i, err := strconv.ParseInt(strings.TrimSuffix(v, TIME_S), 10, 0)
+	//    return time.Duration(i) * time.Second, err
+	//}
 }
 
 func ExecCommand(commandName string, params ...string) bool {
 
-    cmd := exec.Command(commandName, params...)
+	cmd := exec.Command(commandName, params...)
 
-    //显示运行的命令
-    fmt.Println(commandName, cmd.Args)
+	//显示运行的命令
+	fmt.Println(commandName, cmd.Args)
 
-    stdout, err := cmd.StdoutPipe()
+	stdout, err := cmd.StdoutPipe()
 
-    if err != nil {
-        fmt.Println(err)
-        return false
-    }
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
 
-    cmd.Start()
+	cmd.Start()
 
-    reader := bufio.NewReader(stdout)
+	reader := bufio.NewReader(stdout)
 
-    //实时循环读取输出流中的一行内容
-    for {
-        line, err2 := reader.ReadString('\n')
-        if err2 != nil || io.EOF == err2 {
-            fmt.Println("exit cmd start.")
-            break
-        }
-        fmt.Print(line)
-    }
-    err = cmd.Process.Signal(os.Kill)
-    fmt.Println(err)
-    cmd.Wait()
-    return true
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			fmt.Println("exit cmd start.")
+			break
+		}
+		fmt.Print(line)
+	}
+	err = cmd.Process.Signal(os.Kill)
+	fmt.Println(err)
+	cmd.Wait()
+	return true
+}
+
+func GetCurrentFilePath(fileName string, skip int) string {
+	//获取当前函数Caller reports，取得当前调用对应的文件
+	_, f, _, _ := runtime.Caller(skip)
+	//解析出所在目录
+	dir := filepath.Dir(f)
+	//组装配置文件路径
+	file := filepath.Join(dir, fileName)
+	return file
 }
