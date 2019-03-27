8fa6aa11fa631d183d16d6e094df8e7941d44876
tietang <wtt.2012@gmail.com>
2019-03-27 17:46:50 +0800

    remove unkown/log lib

--- example/zk.go
+++ example/zk.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "github.com/samuel/go-zookeeper/zk"
-    zkvs "github.com/tietang/props/zk"
-    "time"
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	zkvs "github.com/tietang/props/zk"
+	"time"
 )
 
-func main() {
+func main_() {
 
-    context := "/config_kv/app1/dev"
+	context := "/config_kv/app1/dev"
 
-    c, ch, err := zk.Connect([]string{"172.16.1.248:2181"}, 2*time.Second)
-    if err != nil {
-        panic(err)
-    }
-    for {
-        event := <-ch
-        fmt.Println(event)
-        if event.State == zk.StateConnected {
-            break
-        }
-    }
+	c, ch, err := zk.Connect([]string{"172.16.1.248:2181"}, 2*time.Second)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		event := <-ch
+		fmt.Println(event)
+		if event.State == zk.StateConnected {
+			break
+		}
+	}
 
-    z := zkvs.NewZookeeperConfigSource("zk", true, context, c)
-    v, err := z.Get("app.xx.key-0")
-    fmt.Println(v)
-    fmt.Println(err)
-    z.Watch("app/xx/key-0", func(children []string, event zk.Event) {
-        fmt.Println("Watch:   ", event, len(z.Keys()))
-        for _, key := range z.Keys() {
-            fmt.Println("new value:  ", key, "= ", z.GetDefault(key, key))
+	z := zkvs.NewZookeeperConfigSource("zk", true, context, c)
+	v, err := z.Get("app.xx.key-0")
+	fmt.Println(v)
+	fmt.Println(err)
+	z.Watch("app/xx/key-0", func(children []string, event zk.Event) {
+		fmt.Println("Watch:   ", event, len(z.Keys()))
+		for _, key := range z.Keys() {
+			fmt.Println("new value:  ", key, "= ", z.GetDefault(key, key))
 
-        }
-    })
-    z.WatchChildren("app/xx", func(children []string, event zk.Event) {
+		}
+	})
+	z.WatchChildren("app/xx", func(children []string, event zk.Event) {
 
-        fmt.Println("WatchChildren:   ", event, len(z.Keys()))
-        for _, key := range z.Keys() {
-            fmt.Println("new value:  ", z.GetDefault(key, key))
-        }
-    })
-    for {
-        event := <-ch
-        fmt.Println(event)
+		fmt.Println("WatchChildren:   ", event, len(z.Keys()))
+		for _, key := range z.Keys() {
+			fmt.Println("new value:  ", z.GetDefault(key, key))
+		}
+	})
+	for {
+		event := <-ch
+		fmt.Println(event)
 
-    }
+	}
 }
